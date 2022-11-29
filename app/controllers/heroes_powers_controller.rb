8a87f0c80b1e3932eee14fa23dbbd71c5cc18a92
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2022-11-29 16:01:51 +0300

    set up controllers

---
+++ app/controllers/heroes_powers_controller.rb
@@ -0,0 +1,2 @@
+class HeroesPowersController < ApplicationController
+end
---
+++ test/controllers/heroes_powers_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class HeroesPowersControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
