cac7090498b5ef621bd986278823f1792dea072c
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2022-11-29 16:14:55 +0300

    add index & show to heroes

--- app/controllers/heroes_controller.rb
+++ app/controllers/heroes_controller.rb
@@ -1,2 +1,19 @@
 class HeroesController < ApplicationController
+
+    def index 
+        @heroes = Hero.all
+        render json: @heroes,status: :ok
+    end
+
+    def show
+        @hero = Hero.find(params[:id])
+        if @hero
+            render json: @hero,status: :ok, serializer: HeroPowerSerializer
+        else
+            render json: {error: "Hero not found"}, status: :not_found
+        end
+    end
+
+
+
 end
